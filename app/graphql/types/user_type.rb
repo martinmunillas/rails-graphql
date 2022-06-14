4e729a4fc9f83926122129262085c8914bd53ac3
martinmunillas <martinmunillas@gmail.com>
2022-06-14 17:09:42 -0400

    feat: create users

---
+++ app/graphql/types/user_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String
+    field :name, String
+    field :posts, [Types::PostType], null: false
+    field :posts_count, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+
+    def posts_count
+      object.posts.size
+    end
+  end
+end
